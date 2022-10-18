1338e7bc75557f786d5da3efaa9da4639444331e
yanyeef <yanyeef@nycstudents.net>
2022-10-18 18:59:00 -0400

    changes

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.print("Enter coordinate 1: "  );
+        String one =  scan.nextLine();
+        System.out.print("Enter coordinate 2: "  );
+        String two = scan.nextLine();
+
+        int xOne = Integer.parseInt(one, 2, one.indexOf(","), 10);
+        int yOne = Integer.parseInt(one, one.indexOf(", ") +2, two.length()-1 ,10);
+        int xtwo = Integer.parseInt(two, 2, two.indexOf(","), 10);
+        int ytwo = Integer.parseInt(two, two.indexOf(", ") +2, two.length()-1 ,10);
+
+        System.out.println("The two points are (" + xOne+ ", " + yOne + ") and (" + xtwo + ", " + ytwo + ")");
+    }
+
 }
