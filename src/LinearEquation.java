6c9d44ebf14e59089d84e898f45ed13e5927697d
yanyeef <yanyeef@nycstudents.net>
2022-10-18 20:42:02 -0400

    changes

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,18 @@
 public class LinearEquation {
+
+    //instance variable
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+
+
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -9,12 +9,17 @@ public class LinearEquationRunner {
         System.out.print("Enter coordinate 2: "  );
         String two = scan.nextLine();
 
-        int xOne = Integer.parseInt(one, 2, one.indexOf(","), 10);
-        int yOne = Integer.parseInt(one, one.indexOf(", ") +2, two.length()-1 ,10);
-        int xtwo = Integer.parseInt(two, 2, two.indexOf(","), 10);
-        int ytwo = Integer.parseInt(two, two.indexOf(", ") +2, two.length()-1 ,10);
 
-        System.out.println("The two points are (" + xOne+ ", " + yOne + ") and (" + xtwo + ", " + ytwo + ")");
+        int xOne = Integer.parseInt(one, 1, one.indexOf(","), 10);
+        int yOne = Integer.parseInt(one, one.indexOf(", ") +2, one.length()-1 ,10);
+        int xTwo = Integer.parseInt(two, 1, two.indexOf(","), 10);
+        int yTwo = Integer.parseInt(two, two.indexOf(", ") +2, two.length()-1 ,10);
+
+        LinearEquation equ = new LinearEquation(xOne,yOne,xTwo,yTwo);
+        equ.lineInfo();
+
+
+        System.out.println("The two points are (" + xOne+ ", " + yOne + ") and (" + xTwo + ", " + yTwo + ")");
     }
 
 }
